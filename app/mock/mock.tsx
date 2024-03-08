c81d80a4cd08b5b7e1399826f7f5fdf36c8b6f25
Hector <torrezhectorb@gmail.com>
2024-03-08 13:58:14 -0600

    refactor: Add mock data for routes

---
+++ app/mock/mock.tsx
@@ -0,0 +1,37 @@
+import { DataType } from "../types/routes";
+
+export const routes: DataType[] = [
+  {
+    key: "1",
+    state: true,
+    type: "Rural",
+    name: "Nombre de la ruta",
+    origin: "Origen de la ruta",
+    destiny: "Destino de la ruta",
+  },
+
+  {
+    key: "2",
+    state: false,
+    type: "Urbano",
+    name: "Nombre de la ruta 2",
+    origin: "Origen de la ruta 2",
+    destiny: "Destino de la ruta 2",
+  },
+  {
+    key: "3",
+    state: true,
+    type: "Rural",
+    name: "Nombre de la ruta 3",
+    origin: "Origen de la ruta 3",
+    destiny: "Destino de la ruta 3",
+  },
+  {
+    key: "4",
+    state: false,
+    type: "Urbano",
+    name: "Nombre de la ruta 4",
+    origin: "Origen de la ruta 4",
+    destiny: "Destino de la ruta 4",
+  },
+];
